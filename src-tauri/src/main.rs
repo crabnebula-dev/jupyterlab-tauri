347ccf21046f5236e7ca4b8ea4c9c527e4892d66
Lucas Nogueira <lucas@tauri.studio>
2023-01-24 12:32:53 -0300

    initial code

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,125 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::{
+    io::BufReader,
+    process::{Child, Command, Stdio},
+    sync::Mutex,
+};
+
+use anyhow::Result;
+use tauri::{
+    api::http::{ClientBuilder, HttpRequestBuilder},
+    AppHandle, Manager, RunEvent, WindowBuilder,
+};
+
+struct JupyterProcess {
+    child: Mutex<Child>,
+    port: u16,
+    token: String,
+}
+
+impl JupyterProcess {
+    async fn stop(&self) -> Result<()> {
+        #[cfg(not(windows))]
+        {
+            let client = ClientBuilder::new().build()?;
+            let request = HttpRequestBuilder::new(
+                "POST",
+                format!(
+                    "http://localhost:${}/api/shutdown?_xsrf=${}",
+                    self.port, self.token
+                ),
+            )?
+            .header("Authorization", format!("token {}", self.token))?;
+            client.send(request).await?;
+        }
+
+        // on Windows, jupyertlab-desktop actually uses `taskkill /PID $PID /T /F`
+        let _ = self.child.lock().unwrap().kill();
+
+        Ok(())
+    }
+}
+
+fn main() {
+    // On macOS and Linux the PATH env variable a packaged app gets does not
+    // contain all the information that is usually set in .bashrc, .bash_profile, etc.
+    // The `fix-path-env` crate fixes the PATH variable
+    let _ = fix_path_env::fix();
+
+    tauri::Builder::default()
+        .setup(|app| {
+            let port = portpicker::pick_unused_port()
+                .ok_or_else(|| anyhow::anyhow!("failed to pick unused port"))?;
+            let token = rand::random::<usize>().to_string();
+
+            let child = start_jupyter_lab(app.handle(), port, token.clone())?;
+            app.manage(JupyterProcess {
+                child: Mutex::new(child),
+                port,
+                token,
+            });
+            Ok(())
+        })
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app, event| {
+            if let RunEvent::Exit = event {
+                let jupyter = app.state::<JupyterProcess>();
+                let _ = tauri::async_runtime::block_on(jupyter.stop());
+            }
+        });
+}
+
+fn start_jupyter_lab(app: AppHandle, port: u16, token: String) -> Result<Child> {
+    let mut child = Command::new("python")
+        .args([
+            "-m",
+            "jupyterlab",
+            "--no-browser",
+            "--expose-app-in-browser",
+            // do not use any config file
+            r#"--JupyterApp.config_file_name="""#,
+            &format!("--ServerApp.port={port}"),
+            r#"--ServerApp.allow_origin=" * ""#,
+            // enable hidden files (let user decide whether to display them)
+            "--ContentsManager.allow_hidden=True",
+        ])
+        .env("JUPYTER_TOKEN", &token)
+        .stderr(Stdio::piped())
+        .spawn()?;
+
+    let mut stderr = child.stderr.take().map(BufReader::new).unwrap();
+    let mut running = false;
+    std::thread::spawn(move || {
+        let mut buf = Vec::new();
+        loop {
+            buf.clear();
+            match tauri::utils::io::read_line(&mut stderr, &mut buf) {
+                Ok(s) if s == 0 => break,
+                _ => (),
+            }
+            let message = String::from_utf8_lossy(&buf);
+            println!("{message}");
+            if message.contains("is running at") && !running {
+                let _ = WindowBuilder::new(
+                    &app,
+                    "main",
+                    tauri::WindowUrl::External(
+                        format!("http://localhost:{port}/lab?token={token}")
+                            .parse()
+                            .unwrap(),
+                    ),
+                )
+                .title("JupyterLab")
+                .build();
+                running = true;
+            }
+        }
+    });
+
+    Ok(child)
+}
