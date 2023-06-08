75a9be57af0e34509ce8ae2c0eb633e557e7ae0b
Johann Derdak <johann@crabnebula.dev>
2023-06-08 17:12:01 +0200

    [TASK] update ui / added a modal for the "installation in progress"

--- src/lib/SetupEnv.svelte
+++ src/lib/SetupEnv.svelte
@@ -2,6 +2,8 @@
   import { invoke, transformCallback } from "@tauri-apps/api/tauri";
   import { relaunch } from "@tauri-apps/api/process";
   import { Circle } from "svelte-loading-spinners";
+  import logo from "./assets/jupyterlab-wordmark.svg";
+  import pythonLogo from "./assets/python.svg";
 
   let installPath = window.__PYTHON_ENV_INSTALL_PATH__;
   let installing = false;
@@ -55,31 +57,122 @@
   }
 </script>
 
-<div>
-  <div>
-    Failed to find a compatible Python environment at the configured path "{installPath}"
-    <button class="install" on:click={installAndRestart}>
-      {#if installing}
-        <Circle size="10" />
-      {/if}
+<div class="content">
+  <div class="logo">
+    <img alt="JupyterLab Logo" width="300" src={logo} />
+  </div>
+  <div class="main">
+    <h1>Getting started</h1>
+    <p>Failed to find a compatible Python environment at the configured path:</p>
+    <p>"<i>{installPath}</i>"</p>
+    <p><strong>Please try to intall it</strong></p>
+    <button disabled={installing} class="install" on:click={installAndRestart}>
+      <img alt="Python Logo" width="30" src={pythonLogo} />
       Install and restart
     </button>
   </div>
 
-  <div class="output">
-    {#each output as line}
-      <div>
-        {line}
-      </div>
-    {/each}
-  </div>
+  <dialog open={installing}>
+    <h1>
+      Installing Python Environment...
+    </h1>
+    <div class="circle">
+      <span>
+        <Circle size="40" />
+      </span>
+    </div>
+    <p>
+      <i>Do not close this window, the application will restart after a successfull install. This may take a couple of minutes.</i>
+    </p>
+    <div class="output">
+      {#each output as line}
+        <div>
+          {line}
+        </div>
+      {/each}
+    </div>
+  </dialog>
 </div>
 
 <style>
+  :root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    vertical-align: middle;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    gap: 4rem;
+    padding: 4rem;
+    position: relative;
+  }
+  .main h1 {
+    margin-bottom: 2rem;
+    margin-top: 0;
+  }
+  button.install img {
+    filter: invert(0.8);
+  }
   button.install {
     display: flex;
     flex-direction: row;
     align-items: center;
-    gap: 4px;
+    gap: 1rem;
+
+    border: none;
+    color: white;
+    cursor: pointer;
+    background-color: transparent;
+    font-size: 18px;
+    padding: 0.5rem;
+  }
+  button:disabled {
+    filter: blur(1px);
+  }
+
+  button.install:hover {
+    background-color: #424242;
+    box-shadow: 1px 6px 21px #424242;
+    border-radius: 3px;
+  }
+
+  dialog {
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -ms-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    padding: 4rem 4rem;
+    width: 60%;
+    background-color: #424242;
+    box-shadow: 0 0 79px #424242;
+    color: white;
+    border: none;
+    border-radius: 8px;
+  }
+  dialog h1 {
+    margin-bottom: 3rem;
+    text-align: center;
+    line-height: 1;
+    }
+
+  .circle {
+    display: flex;
+    margin-bottom: 3rem;
+  }
+
+  .circle span {
+    display: inline-block;
+    margin: 0 auto;
+  }
+
+  dialog::backdrop {
+    background-color: black;
   }
 </style>
